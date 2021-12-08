da746c62907b7d6338a605bbac6836f1edf6cc82
mickael.fernandes.0@gmail.com <mickael.fernandes.0@gmail.com>
2021-12-08 13:32:50 +0100

    Filtre pour ajouter un header HTTP

--- src/main/java/fr/lernejo/todo/TodoListController.java
+++ src/main/java/fr/lernejo/todo/TodoListController.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
         @PostMapping("/api/todo")
         public void addTodo (@RequestParam("message") String message, @RequestParam("author") String author){
             this.todoList.add(new Todo(message, author));
+            System.out.println(todoList);
         }
 
         @GetMapping("/api/todo")
