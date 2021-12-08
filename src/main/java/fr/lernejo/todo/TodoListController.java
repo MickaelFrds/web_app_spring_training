6fa7a9a0c59921a85abb731b7d835c3aae78c18e
mickael.fernandes.0@gmail.com <mickael.fernandes.0@gmail.com>
2021-12-08 13:11:09 +0100

    Endpoints HTTP

---
+++ src/main/java/fr/lernejo/todo/TodoListController.java
@@ -0,0 +1,23 @@
+package fr.lernejo.todo;
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+
+    @RestController
+    public class TodoListController {
+
+        private final ArrayList<Todo> todoList;
+
+        public TodoListController() {
+            this.todoList = new ArrayList<Todo>();
+        }
+
+        @PostMapping("/api/todo")
+        public void addTodo (@RequestParam("message") String message, @RequestParam("author") String author){
+            this.todoList.add(new Todo(message, author));
+        }
+
+        @GetMapping("/api/todo")
+        public ArrayList<Todo> getTodos(){
+            return this.todoList;
+        }
+}
